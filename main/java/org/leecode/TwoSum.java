95e331acf5d32d7cb918e7c5ac3486973fc3b63d
wangguijie <834919155@qq.com>
2025-09-01 00:26:30 +0800

    两数相加

--- main/java/org/leecode/TwoSum.java
+++ main/java/org/leecode/TwoSum.java
@@ -1,22 +1,24 @@
 package org.leecode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
-        java.util.HashMap<Integer,Integer> hashMap = new java.util.HashMap<>();
+        java.util.HashMap<Integer, Integer> hashMap = new java.util.HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int tmp = target - nums[i];
-            if (hashMap.containsKey(tmp)){
-                return new int[]{hashMap.get(tmp),i};
+            if (hashMap.containsKey(tmp)) {
+                return new int[]{hashMap.get(tmp), i};
             }
-            hashMap.put(nums[i],i);
+            hashMap.put(nums[i], i);
         }
-        return new int[]{-1,-1};
+        return new int[]{-1, -1};
     }
 
 
     public static void main(String[] args) {
-
+        int[] ints = twoSum(new int[]{2, 7, 11, 15}, 9);
+        System.out.println("ints:" + Arrays.toString(ints));
     }
 }
